475ea01b0b7d03eb2fc7d0f21d1614dbeaf12b25
eric-volz <eric.volz02@gmail.com>
2021-08-03 01:28:29 +0200

    Löschen des Imports im TestFile

--- test/testFile.py
+++ test/testFile.py
@@ -1,7 +1,5 @@
-import TelegramBot as tb
 
-
-def test():
+def test(tb):
     token = "1869353284:AAGmgbwOaGr3CWYitRG1ULsNG0DiI4SBewQ"
 
     bot = tb.TelegramBotManager(token, tb.TelegramBotManager.getChatID(token), 1)
